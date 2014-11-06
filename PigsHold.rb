a50b9b0844240e0f31ccba0cfb66958e7aee98ac
ttibble90 <thomasdtibble@lewisu.edu>
2014-11-06 14:18:24 -0600

    Upload Files

---
+++ PigsHold.rb
@@ -0,0 +1,17 @@
+#Pigs Hold at 20
+#Tom Tibble and Kevin
+
+cpuTotal = 0
+roundTotal = 0
+until roundTotal  >= 20
+
+	roll = rand(6) + 1
+	puts("Roll: #{roll}")
+
+	if roll == 1
+		roundTotal = 0
+		break
+	end
+	roundTotal += roll
+end
+puts("Round total: #{roundTotal}")
