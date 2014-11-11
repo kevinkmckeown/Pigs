fc1a99a3cbc5715d972a7dec2ede0f894d8f5c95
kevinkmckeown <kevinkmckeown@lewisu.edu>
2014-11-11 16:58:34 -0600

    Finished formatting output

--- pigProb.rb
+++ pigProb.rb
@@ -4,7 +4,7 @@
 round_total = 0
 print "Enter a hold number:"
 hold = gets.chomp
-print "Enter the number of simulations:"
+print "Hold-at-20 turn simulations?"
 run_count = gets.chomp
 hold = hold.to_i
 run_count = run_count.to_i
@@ -14,7 +14,6 @@ for i in 1..run_count
 	until round_total  >= hold
 
 		roll = rand(6) + 1
-		#puts("Roll: #{roll}")
 
 		if roll == 1
 			round_total = 0
@@ -22,8 +21,9 @@ for i in 1..run_count
 		end
 		round_total += roll
 	end
-	#puts("Round total: #{round_total}")
 	scores [round_total] += 1
 	round_total = 0
 end
-	puts scores
+puts "Score	Estimated Probability"
+scores = Hash[scores.sort_by{|key,value| key}]
+scores.each{ |key,value| puts("#{key}	#{((value.to_f/run_count)).round(3)}") }
